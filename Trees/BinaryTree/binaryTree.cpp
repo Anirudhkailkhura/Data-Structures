33dbcff63e25d5f246862f44cd7962a88917bcf6
anirudh <anirudhkailkhura@gmail.com>
2023-10-05 18:45:27 +0530

    commit

--- Trees/BinaryTree/binaryTree.cpp
+++ Trees/BinaryTree/binaryTree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 class Node
@@ -14,13 +15,84 @@ public:
         left = right = NULL;
     }
 };
+// DFS
+void preorderTraversal(Node *rootNode)
+{
+    // base case
+    if (rootNode == NULL)
+    {
+        return;
+    }
+    cout << rootNode->value << " ";
+    // resursive call
+    preorderTraversal(rootNode->left);
+    preorderTraversal(rootNode->right);
+}
+
+void inorderTraversal(Node *rootNode)
+{
+
+    // base case
+    if (rootNode == NULL)
+    {
+        return;
+    }
+    // resursive call
+    inorderTraversal(rootNode->left);
+    cout << rootNode->value << " ";
+    inorderTraversal(rootNode->right);
+}
+
+void postorderTraversal(Node *rootNode)
+{
+
+    // base case
+    if (rootNode == NULL)
+    {
+        return;
+    }
+    // resursive call
+    postorderTraversal(rootNode->left);
+    postorderTraversal(rootNode->right);
+    cout << rootNode->value << " ";
+}
+
+void levelOrderTraversal(Node* rootNode){
+    if(rootNode == NULL){
+        return;
+    }
+    queue<Node*> q;
+    q.push (rootNode);
+
+    while(!q.empty()){
+        int nodeAtCurrLevel = q.size();
+        while(nodeAtCurrLevel--){
+            Node* currNode = q.front();
+            q.pop();
+            cout<<currNode->value<<" ";
+
+            if(currNode->left){
+                q.push(currNode->left);
+            }
+
+            if(currNode->right){
+                q.push(currNode->right);
+            }
+        }
+        cout<<endl;
+    }
+
+     
+}
+
 int main()
 {
-    Node *root = new Node(2);
-    root->left = new Node(3);
-    root->right = new Node(4);
+    Node *rootNode = new Node(2);
+    rootNode->left = new Node(3);
+    rootNode->right = new Node(4);
+    rootNode->left->left = new Node(6);
+    rootNode->left->right = new Node(8);
+    rootNode->right->right = new Node(9);
 
-    cout << "Root node :" << root->value << endl;
-    cout << "Left child:" << root->left->value << endl;
-    cout << "right chilld:" << root->right->value << endl;
+    levelOrderTraversal (rootNode);
 }
